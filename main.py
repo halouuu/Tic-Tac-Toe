b7e1c51e940506356b46e5c4ea7bb5fd2f2eb4ec
halouuu <100639293+halouuu@users.noreply.github.com>
2022-08-03 18:12:27 +1200

    There is a bug and I don't know where it is T_T

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #Importing the follwing modules so I can use them in my code
 import sys
-from turtle import back, pos
-from unittest import runner
 import pygame
 #This module is used for the console board
 import numpy as np
@@ -22,6 +20,109 @@ pygame.display.set_caption("Sophia's L3 Project - AI Tic Tac Toe <3")
 #Putting a background colour
 screen.fill(background_colour)
 
+class Board:
+
+    def __init__ (self):
+        self.squares = np.zeros ((ROWS, COLS))
+        #zeros = empty squares
+        self.empty_squares = self.squares 
+        #a list of empty of empty squares
+        self.marked_squares = 0
+        #It will be 0 at the start because there will be no markled squares at the start of the game.
+
+    def final_state(self, show = False):
+        #return 0 if there is no win yet, but it does not mean that there is a draw.
+        #return 1 if player 1 wins.
+        #return 2 if player 2 wins
+
+        #vertical wins
+        #need to loop through all columns.
+        for col in range (COLS):
+            #this checks if each figure in a column is the same. It can not = 0 because it has to be a marked square. If that is not added in then it will also check empty columns as well. Which would not result in a win.
+            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:
+                if show:
+                    #this will be the colour of the winning line
+                    colour = circle_colour if self.squares[0][col] == 2 else cross_colour
+                    #initial position
+                    iPos = (col * square_size + square_size // 2, 20)
+                    #final position
+                    fPos = (col * square_size + square_size // 2, HEIGHT - 20)
+                    pygame.draw.line(screen, colour, iPos,fPos, line_width)
+                #we want it to return anyone of the squares because they are all the same number. I just chose the first one. 
+                #eg. 1 = 1 = 1 != 0, we return 1 when player one wins.
+                return self.squares[0][col]
+
+        for row in range (ROWS):
+            #this checks if each figure in a row is the same. It can not = 0 because it has to be a marked square. If that is not added in then it will also check empty columns as well. Which would not result in a win.
+            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
+                if show:
+                    #this will be the colour of the winning line
+                    colour = circle_colour if self.squares[row][0] == 2 else cross_colour
+                    #initial position
+                    iPos = (20, row * square_size + square_size // 2)
+                    #final position
+                    fPos = (WIDTH - 20, row * square_size + square_size // 2)
+                    pygame.draw.line(screen, colour, iPos,fPos, line_width)
+                #we want it to return anyone of the squares because they are all the same number. I just chose the first one. 
+                #eg. 1 = 1 = 1 != 0, we return 1 when player one wins.
+                return self.squares[row][0]
+
+        #diagonal wins
+        #negative win
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+            if show:
+                colour = circle_colour if self.squares[1][1] == 2 else cross_colour
+                #initial position
+                iPos = (20,20)
+                #final position
+                fPos = (WIDTH - 20, HEIGHT - 20)
+                pygame.draw.line(screen, colour, iPos, fPos, cross_width)
+            return self.squares[0][0]
+
+        #positve win
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+            if show:
+                colour = circle_colour if self.squares[1][1] == 2 else cross_colour
+                #initial position
+                iPos = (20,HEIGHT - 20)
+                #final position
+                fPos = (WIDTH - 20, 20)
+            return self.squares[0][0]
+
+        #we want to return 0 if there is no win yet so if non of the above if statements are true, we return 0.
+        return 0
+
+
+    def mark_square (self, row, col, player):
+        #there will be two players: 1, 2. When a square is marked, the square on console board will not be zero anymore, it will be replaced with the player number (1 or 2)
+        self.squares[row][col] = player
+        #this will increase the amount of marked squares which will help us know when the board is full
+        self.marked_squares += 1
+
+    def empty_square(self, row, col):
+        #if this is true, then the square is empty
+        return self.squares[row][col] == 0
+
+    def isfull(self):
+        return self.marked_squares == 9
+        #if the marked_squares = 9, the board is full, so the function is true.
+
+    def isempty(self):
+        return self.marked_squares == 0
+        #if the marked_squares = 0, the board is empty, so the function is true.
+
+    def get_empty_squares (self):
+        #empty squares will be a list which co-ordinates will be appended to.
+        empty_squares = []
+        #classic double for loop
+        for row in range(ROWS):
+            for col in range(COLS):
+                #if a square is empty, it will get appended into the empty squares list.
+                if self.empty_squares(row,col):
+                    empty_squares.append((row,col))
+
+        return empty_squares
+
 class AI:
     #there will be 3 levels:
         # 0 = random AI: the ai will just go whereever, no strategy involved.
@@ -33,14 +134,14 @@ class AI:
         self.level = level
         self.player = player
 
-    def  random(self, board):
+    def random(self, board):
         #creating a new list. This function will return all of the empty squares.
         empty_squares = board.get_empty_squares()
         #the move that the AI will make.
         index = random.randrange(0,len(empty_squares))
 
         #it will return in form of (row,col)
-        return empty_squares(index)
+        return empty_squares[index]
 
     #the board is the board that the algorithm is analysing. Maximising utilizes the minimax algorithm, will return true or false.
     #AI is player two and is trying to minimize because maximising is false. Ref. evaluation function.
@@ -78,15 +179,15 @@ class AI:
                 #this time it will use temp_board as the board instead of main_board. It will return an evaluation and a best move.
                 #we just want the evaluation, because the (row,col) will be the move which leads to the evaluation.
                 #[0] is the first position which will be the evaluation.
-                evaluation = self.minimax(temp_board, False)[0]
+                eval = self.minimax(temp_board, False)[0]
                 #need to check if the evalution is greater than max_eval (defult is -2). 
                 #The evaluation can only be -1, 0, 1. Therefore, the evaluation will always be greater than the max_eval.
-                if evaluation > max_eval:
-                    max_eval = evaluation
+                if eval > max_eval:
+                    max_eval = eval
                     #this is the move that lead to the evaluation
                     best_move = (row, col)
 
-            return max, best_move
+            return max_eval, best_move
 
         #this is the AI: trying to not maximise.
         elif not maximising:
@@ -104,110 +205,30 @@ class AI:
                 #this time it will use temp_board as the board instead of main_board. It will return an evaluation and a best move.
                 #we just want the evaluation, because the (row,col) will be the move which leads to the evaluation.
                 #[0] is the first position which will be the evaluation.
-                evaluation = self.minimax(temp_board, True)[0]
+                eval = self.minimax(temp_board, True)[0]
                 #need to check if the evalution is less than min_eval (defult is 2). 
                 #The evaluation can only be -1, 0, 1. Therefore, the evaluation will always be less than the min_eval.
-                if evaluation < min_eval:
-                    min_eval = evaluation
+                if eval < min_eval:
+                    min_eval = eval
                     #this is the move that lead to the evaluation
                     best_move = (row, col)
 
             return min_eval, best_move
 
-    def evalution(self, main_board):
+    def eval(self, main_board):
         if self.level == 0:
             #random choice
-            evaluation = 'random'
+            eval = 'random'
             move = self.random(main_board)
         else:
             #minimax algorithm
             #will return evaluation and move. 
-            evaluation, move = self.minimax(main_board, False)
+            eval, move = self.minimax(main_board, False)
     
-        print (f"AI has chosen to mark the square in position {move} with an evaluation of {evaluation}")
+        print (f"AI has chosen to mark the square in position {move} with an evaluation of {eval}")
 
         #it will return in form of (row,col)
         return move 
-
-class Board:
-
-    def __init__ (self):
-        self.squares = np.zeros ((ROWS, COLS))
-        #zeros = empty squares
-        self.empty_squares = self.squares 
-        #a list of empty of empty squares
-        self.marked_squares = 0
-        #It will be 0 at the start because there will be no markled squares at the start of the game.
-
-    def final_state(self, show = False):
-        #return 0 if there is no win yet, but it does not mean that there is a draw.
-        #return 1 if player 1 wins.
-        #return 2 if player 2 wins
-
-        #vertical wins
-        #need to loop through all columns.
-        for col in range (COLS):
-            #this checks if each figure in a column is the same. It can not = 0 because it has to be a marked square. If that is not added in then it will also check empty columns as well. Which would not result in a win.
-            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:
-                if show:
-                    #this will be the colour of the winning line
-                    colour = circle_colour if self.squares[0][col] == w else cross_colour
-                    #initial position
-                    iPos = (col * square_size + square_size // 2, 20)
-                    pygame.draw.line(screen, colour, iPos,fPos, line_width)
-                #we want it to return anyone of the squares because they are all the same number. I just chose the first one. 
-                #eg. 1 = 1 = 1 != 0, we return 1 when player one wins.
-                return self.squares[0][col]
-
-        #horizontal wins
-        #need to loop through all rows.
-        for row in range (ROWS):
-            #this checks if each figure in a row is the same. It can not = 0 because it has to be a marked square. If that is not added in then it will also check empty rows as well. Which would not result in a win.
-            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
-                #we want it to return anyone of the squares because they are all the same number. I just chose the first one. 
-                #eg. 2 = 2 = 2 != 0, we return 2 when player two wins.
-                return self.squares[row][0]
-
-        #diagonal wins
-        #negative win
-        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
-            return self.squares[0][0]
-
-        #positve win
-        if self.squares[2][2] == self.squares[1][1] == self.squares[0][0] != 0:
-            return self.squares[0][0]
-
-        #we want to return 0 if there is no win yet so if non of the above if statements are true, we return 0.
-        return 0
-
-
-    def mark_square (self, row, col, player):
-        #there will be two players: 1, 2. When a square is marked, the square on console board will not be zero anymore, it will be replaced with the player number (1 or 2)
-        self.squares[row][col] = player
-        #this will increase the amount of marked squares which will help us know when the board is full
-        self.marked_squares += 1
-
-    def empty_square(self, row, col):
-        #if this is true, then the square is empty
-        return self.squares[row][col] == 0
-
-    def isfull(self):
-        return self.marked_squares == 9
-        #if the marked_squares = 9, the board is full, so the function is true.
-
-    def isempty(self):
-        return self.marked_squares == 0
-        #if the marked_squares = 0, the board is empty, so the function is true.
-
-    def get_empty_squares (self):
-        #empty squares will be a list which co-ordinates will be appended to.
-        empty_squares = []
-        #classic double for loop
-        for row in range(ROWS):
-            for col in range(COLS):
-                #if a square is empty, it will get appended into the empty squares list.
-                if self.empty_squares(row,col):
-                    empty_squares.append(row,col)
 
 class Game:
 
@@ -225,15 +246,6 @@ class Game:
         self.gamemode = 'ai' #pvp or ai
         #if the game is not over. Game is over when there is a draw or a player wins.
         self.running = True
-
-    def make_move(self):
-        #mark the square so we know it is not empty
-        self.board.mark_square(row, col, self.player)
-        #draw the figure so that the player knows where the other player chose.
-        self.draw_fig(row,col)
-        #switch turns
-        self.next_turn()
-
 
     #to show the game board lines
     def show_lines (self):
@@ -271,6 +283,15 @@ class Game:
             pygame.draw.circle(screen , circle_colour, center, radius, circle_width)
 
     #to switch players
+
+    def make_move(self, row, col):
+        #mark the square so we know it is not empty
+        self.board.mark_square(row, col, self.player)
+        #draw the figure so that the player knows where the other player chose.
+        self.draw_fig(row,col)
+        #switch turns
+        self.next_turn()
+    
     def next_turn(self):
         self.player = self.player % 2 + 1
         ''' example:
@@ -287,7 +308,7 @@ class Game:
         #if the final_state != 0, then that means that someone has won.
         #if the board is full, then there is a draw.
         #if either of those are true then it will return true.
-        return self.board.final_state() != 0 or self.board.isfull()
+        return self.board.final_state(show = True) != 0 or self.board.isfull()
 
     def reset (self):
         #this just restarts all of the attributes to all defult values.
@@ -344,15 +365,18 @@ def main():
 
                         if game.isover():
                             game.running = False
-                     
+
         if game.gamemode == 'ai' and game.player == ai.player and game.running:
             #update the screen so the player can see the updated screen with the AI move before the AI makes another move.
             pygame.display.update()
 
             #AI methods
             #ai.evaluation will return a move in the form of (row,col) which will be saved into 2 variables.
-            row,col = ai.evaluation(board)
+            row,col = ai.eval(board)
             game.make_move(row,col)
+
+            if game.isover():
+                game.running = False
                         
        
         #This updates the display
